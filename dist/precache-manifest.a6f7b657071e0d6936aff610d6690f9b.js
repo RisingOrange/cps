f73e0c4dd3d2922fe52e3d71091bc6410a0a361b
RisingOrange <jakub.fidler@protonmail.com>
2021-02-13 16:10:15 +0100

    "Initial dist subtree commit"

---
+++ dist/precache-manifest.a6f7b657071e0d6936aff610d6690f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "877b12b96722466e3e71",
+    "url": "/css/app.5463e1e8.css"
+  },
+  {
+    "revision": "0f9c51390edbb62f320e36b3f4ac4b3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "877b12b96722466e3e71",
+    "url": "/js/app.070f75c7.js"
+  },
+  {
+    "revision": "799db7e3f09ca0ee4818",
+    "url": "/js/chunk-vendors.dc2beb86.js"
+  },
+  {
+    "revision": "44f42bec8992f5558c370737c4b2fe8e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
