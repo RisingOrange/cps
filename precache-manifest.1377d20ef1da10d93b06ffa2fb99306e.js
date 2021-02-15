2769a199fb70570e3c459d82924c9b5d38cd54a6
RisingOrange <jakub.fidler@protonmail.com>
2021-02-15 20:31:02 +0100

    Updates

---
+++ precache-manifest.1377d20ef1da10d93b06ffa2fb99306e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5877fac068cfb62ec62f",
+    "url": "/css/app.e70031f4.css"
+  },
+  {
+    "revision": "2dff68c9d231e971a7f2819a5c28c845",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5877fac068cfb62ec62f",
+    "url": "/js/app.99cfffcf.js"
+  },
+  {
+    "revision": "fef5a9d5f161333c3746",
+    "url": "/js/chunk-vendors.f1affdc0.js"
+  },
+  {
+    "revision": "44f42bec8992f5558c370737c4b2fe8e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
